b4d32175a582bd1d3b7e828aa521791d604e35e3
kawany-santos <kawany.santos.etec@gmail.com>
2026-06-08 16:38:57 -0300

    responsividade1

--- js/grafico.js
+++ js/grafico.js
@@ -2,13 +2,13 @@ let circularProgress = document.querySelector(".circular-progress"),
     progressValue = document.querySelector(".progress-value");
 
 let progressStartValue = 0,
-    progressEndValue = 90,
+    progressEndValue = 65,
     speed = 100;
 
 let progress = setInterval(() => {
     progressStartValue++;
 
-    circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`;
+    circularProgress.style.background = `conic-gradient(#01B574 ${progressStartValue * 3.6}deg, #060C2B  0deg)`;
 
     if(progressStartValue == progressEndValue){
         clearInterval(progress);
